dd6e14493711d5163849e00e03d1381117929b6e
Kaley Regner <66963723+kalesreg@users.noreply.github.com>
2021-04-01 13:21:16 -0400

    update to Programming Lab 7

---
+++ groceries_list.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  1 12:29:44 2021
+groceries_list.py
+@author: kales
+"""
+import csv
+def main():
+    # Buying groceries in the express lange with a LIMIT of 20 ITEMS
+    sum_prices = 0.0
+    num_items = 0
+    
+    print("How many items do you want to buy?")
+    print("Remember at least 1 and no more than 20 items in express lane.")
+    num_items = validate_range(num_items, 1, 20)
+    items = [" "] * num_items
+    qty = [0] * num_items
+    price = [0.0] * num_items
+    total_price = [0.0] * num_items
+    
+    # Read data into the lists
+    for i in range(0, num_items, 1):
+        print("Please enter Grocery List Item ", (i + 1), ":")
+        items[i] = input()
+        qty[i] = validate_int(qty[i], 0, items[i])
+        price[i] = validate_float(price[i], 0, items[i])
+        total_price[i] = qty[i] * price[i]
+    
+    # Display the grocery list
+    print("%-20s%-20s%5s%15s%15s\n" % ("Item", "Description", "Qty", \
+                                       "Item Price", "Total Price"))
+    display_list(items, qty, price, total_price)
+    
+        
+def validate_range(value, low_range, high_range):
+    while True:
+        try:
+            value = int(input())
+            if not low_range <= value <= high_range: 
+                raise Exception
+            else:
+                break
+        except ValueError:
+            print("You must enter an integer.")
+        except Exception:
+            print("You must enter a value that is between", \
+                  low_range, "and", high_range, ".")
+    return value
+
+def validate_int(value, low_value, description):
+    while True:
+        try:
+            print("Please enter the quantity for", description, ":")
+            value = int(input())
+            if value < low_value: 
+                raise Exception
+            else:
+                break
+        except ValueError:
+            print("You must enter an integer.")
+        except Exception:
+            print("You must enter a value that is at least", low_value, ".")
+    return value
+
+def validate_float(value, low_value, description):
+    while True:
+        try:
+            print("Please enter the price for", description, ":")
+            value = float(input())
+            if value < low_value: 
+                raise Exception
+            else:
+                break
+        except ValueError:
+            print("You must enter an float.")
+        except Exception:
+            print("You must enter a value that is at least", low_value, ".")
+    return value
+
+def display_list(items, qty, price, total_price):
+    try:
+        # By adding 1 to len(items), we will throw an error
+        for i in range(0, len(items), 1):
+            print("%-20s%-20s%5d%15.2f%15.2f\n" % (i, items[i], qty[i], \
+                                              price[i], total_price[i]))
+    except IndexError:
+        print("There was an indexing error.")
+    except Exception as err:
+        print("An error occurred.")
+        print(err)
+
+main()
