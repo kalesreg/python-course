e02bfbf94c653851879cd866ade5e937128c45b5
Kaley Regner <66963723+kalesreg@users.noreply.github.com>
2021-04-22 20:54:12 -0400

    Programming Assignment 3

--- gross_margin.py
+++ gross_margin.py
@@ -4,29 +4,56 @@ Created on Thu Apr 22 02:07:55 2021
 gross_margin.py
 @author: kales
 """
-
-# accepts the user's input from the gm_gui.py file and computes the gross
+# Accepts the user's input from the gm_gui.py file and computes the gross
 # margin in $ and gross margin % and displays it
 
+# Create GrossMargin object
 class GrossMargin:
-    # Create GrossMargin object
     # should have an __init__ method that accepts the cost and sale form the 
     # gm_gui.py file and initializes the cost and sale
+    def __init__(self, cost, sale):
+        self.__cost = float(cost)
+        self.__sale = float(sale)
+    
     # Should have a __str__ method that includes decision structures
-    # First, if cost and sale enteredby the user is 0 for each, then return 
-    # message that says "Nothing from nothing leaves nothing. Try again."
-    # If the cost entered by the user is o, then return a message that says
-    # "Our suppliers never give us stuff for free. Please re-enter."
-    # If the sale entered by the user is 0, then return a message that says
-    # "We don't sell stuff for free. Please re-enter."
-    # If the user enters a value greater than 0 for the cost and greater 
-    # than 0 for the sale, then caluclate the gross_profit_margin by 
-    # subtracting the cost from the sale
-    # Then calculate the gross_profit_percent by taking the gross_profit_margin
-    # and dividing by the sale and then multiplying by 100
-    # A string named display_profit will return the following items back to the
-    # gross_margin_gui file when tested:
-        # Cost in $:   cost
-        # Sale in S:   sale
-        # Gross Margin in $   gross_profit_margin
-        # Gross Margin %   gross_profit_percent
+    def __str__(self):
+        # First, if cost and sale entered by the user is 0 for each, then return 
+        # message that says "Nothing from nothing leaves nothing. Try again."
+        if self.__cost == 0 and self.__sale == 0:
+            return "Nothing from nothing leaves nothing. Try again."
+        # If the cost entered by the user is 0, then return a message that says
+        # "Our suppliers never give us stuff for free. Please re-enter."
+        elif self.__cost == 0:
+            return "Our suppliers never give us stuff for free. Please re-enter."
+        # If the sale entered by the user is 0, then return a message that says
+        # "We don't sell stuff for free. Please re-enter."
+        elif self.__sale == 0:
+            return "We don't sell stuff for free. Please re-enter."
+        # If the user enters a value greater than 0 for the cost and greater 
+        # than 0 for the sale, then caluclate the gross_profit_margin by 
+        # subtracting the cost from the sale. Then calculate the 
+        # gross_profit_percent by taking the gross_profit_margin
+        # and dividing by the sale and then multiplying by 100
+        else:
+            if self.__cost > 0 and self.__sale > 0:
+                self.gross_profit_margin = self.__sale - self.__cost
+                self.gross_profit_percent = self.gross_profit_margin / self.__sale * 100
+            
+            # A string named display_profit will return the following items back to the
+            # gross_margin_gui file when tested:
+            # Cost in $:   cost
+            # Sale in S:   sale
+            # Gross Margin in $   gross_profit_margin
+            # "Gross Margin %"   gross_profit_percent
+            self.display_profit = '%-20s%10.2f' % ('Cost in $:', self.__cost)
+            self.display_profit += '\n%-20s%10.2f' % ('Sale in S:', self.__sale)
+            self.display_profit += '\n%-20s%10.2f' % ('Gross Margin in $', self.gross_profit_margin)
+            self.display_profit += '\n%-20s%10.2f' % ('Gross Margin %', self.gross_profit_percent)
+        
+            return self.display_profit
+
+"""
+# Testing area
+test = GrossMargin(100, 200)
+print(test.__str__())
+"""
--- gross_margin_gui.py
+++ gross_margin_gui.py
@@ -9,28 +9,71 @@ import tkinter
 import gross_margin
 
 # Create a class called gm_gui that is a subclass of the Frame class
+class gm_gui(tkinter.Frame):
+    # Create method that sets up the window and widgets
+    def __init__(self):
+        # Create the main window
+        # Make it automatically run when the window is created
+        tkinter.Frame.__init__(self)
+        
+        # Title should be We <3 $$$ Co.
+        self.master.title('We <3 $$$ Co.')
+        
+        # Set up the grid so that we can arrange the widgets in a 2D table
+        self.grid()
+        
+        # 1st label display "Cost in $:" 
+        self.first_label = tkinter.Label(self, text = 'Cost in $: ', justify = 'left', anchor = 'w')
+        # 1st label displayed left aligned first row, first column
+        self.first_label.grid(sticky = 'w', row = 0, column = 0)
+        
+        # Cost variable is a double
+        self.cost = tkinter.DoubleVar()
+        self.cost_entry = tkinter.Entry(self, textvariable = self.cost)
+        # Entry widget for the cost should be first row, second column
+        self.cost_entry.grid(row = 0, column = 1)
 
-# Create method that sets up the window and widgets
-# Make it automatically run whe the window is created
-# Title should be We <3 $$$ Co.
-# Set up the grid so that we can arrange the widgets in a 2D table
-# 1st label display "Cost in $:" which should be in first row, first column
-# and should be left aligned and stick the W part
-# The sale variable is a double, the entry widget for the cost should be 
-# first row, second column
-# 2nd label display "Sale in $:" and label should be in second row, first column
-# Label should be left aligned, stick to W part of the grid
-# Sale vairable is a double, entry widget for sale should be second row, second column
-# Command button widget will state "Calculate Gross Profit $ and %", when user
-# clicks on it, it should run the compute_gross_profit method
-# Button should be in the fifth row, first column
-# Output Text widget should be 50 characters wide and 10 lines in height
-# and will appear in the sixth row, first column
-# compute_gross_profit method will first delete any text in the output Text
-# widget and then use a variable called display_profit as a string to take
-# all output for the GrossMargin object.
-# Insert contents saved in display_profit string variable into the output
-# Text widget.
+        # 2nd label display "Sale in $:" 
+        self.second_label = tkinter.Label(self, text = 'Sale in $: ', justify = 'left', anchor = 'w')
+        # 2nd label should be in left aligned second row, first column
+        self.second_label.grid(sticky = 'w', row = 1, column = 0)
+        
+        # Sale variable is a double
+        self.sale = tkinter.DoubleVar()
+        self.sale_entry = tkinter.Entry(self, textvariable = self.sale)
+        #entry widget for sale should be second row, second column
+        self.sale_entry.grid(row = 1, column = 1)
+        
+        # Command button widget will state "Calculate Gross Profit $ and %", when user
+        # clicks on it, it should run the compute_gross_profit method
+        self.calculate_button = tkinter.Button(self, \
+                                        text = 'Calculate Gross Profit $ and %', \
+                                        command = self.compute_gross_profit)
+        
+        # Button should be in the fifth row, first column
+        self.calculate_button.grid(row = 4, column = 0, columnspan = 2)
+        
+        # Output Text widget should be 50 characters wide and 10 lines in height
+        self.output = tkinter.Text(self, width = 50, height = 10)
+        
+        # Output will appear in the sixth row, first column
+        self.output.grid(row = 5, column = 0, columnspan = 2)
+        
+    # compute_gross_profit method will first delete any text in the output Text
+    # widget and then use a variable called display_profit as a string to take
+    # all output for the GrossMargin object.
+    # Insert contents saved in display_profit string variable into the output
+    # Text widget.
+    def compute_gross_profit(self):
+        self.output.delete('1.0', 'end')
+        self.gm = gross_margin.GrossMargin(self.cost.get(), self.sale.get())
+        self.display_profit = str(self.gm.__str__())
+        self.output.insert('1.0', self.display_profit)
+        
 # Create a method for main() that creates an instance of the gm_gui and runs
 # the mainloop() method that waits on the event which is the user clicking 
 # the Calculate Gross Profit $ and % command button
+def main():
+    gm_gui().mainloop()
+
+main()
